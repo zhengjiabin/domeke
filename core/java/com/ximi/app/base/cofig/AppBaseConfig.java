a9c786765a8d8409edf64f3d7a82824573e428bb
lijiasen <jiasen.li@gmail.com>
2014-07-07 01:12:18 +0800

    添加druid配置文件读取方式

--- core/java/com/ximi/app/base/cofig/AppBaseConfig.java
+++ core/java/com/ximi/app/base/cofig/AppBaseConfig.java
@@ -1,5 +1,11 @@
 package com.ximi.app.base.cofig;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -10,34 +16,64 @@ import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.plugin.druid.DruidStatViewHandler;
 
 public class AppBaseConfig extends JFinalConfig {
+	
+	
+	private Configuration config;
+	
+	private Logger logger = LoggerFactory.getLogger(AppBaseConfig.class);
+	
 
 	@Override
-	public void configConstant(Constants arg0) {
+	public void configConstant(Constants constants) {
 
 	}
 
 	@Override
 	public void configHandler(Handlers handlers) {
 		DruidStatViewHandler dsvh = new DruidStatViewHandler("/druid");
+		handlers.add(dsvh);
 	}
 
 	@Override
-	public void configInterceptor(Interceptors arg0) {
+	public void configInterceptor(Interceptors interceptors) {
 
 	}
 
 	@Override
 	public void configPlugin(Plugins plugins) {
-		String password = null;
-		String username = null;
-		String url = null;
+		plugins.add(getDruidPlugin());
+	}
+	
+	private DruidPlugin getDruidPlugin(){
+		loadPropertyFile("dataSource.properties");
+		String url = getProperty("jdbc.url");
+		String password = getProperty("jdbc.password");
+		String username = getProperty("jdbc.username");
+		String driveClass = getProperty("jdbc.driverClass");
 		DruidPlugin druidPlugin = new DruidPlugin(url, username, password);
-		plugins.add(druidPlugin);
+		druidPlugin.setDriverClass(driveClass);
+		return druidPlugin;
+		
 	}
 
 	@Override
 	public void configRoute(Routes arg0) {
 
 	}
+	
+	public Configuration loadConfigurationFile(String file) {
+		try {
+			config = new PropertiesConfiguration(file);
+		} catch (ConfigurationException e) {
+			logger.error("Config file loading failed！",e);
+			throw new RuntimeException("Config file loading failed: " + file);
+		}		
+		return config;
+	}
+	
+	public String getProperty(String key) {
+		return config.getString(key);
+	}
+	
 
 }
