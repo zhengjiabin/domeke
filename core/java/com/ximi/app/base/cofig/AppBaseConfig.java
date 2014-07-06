1a2c0b41d98e74dd94e84c6a5442b92ca743cf55
lijiasen <jiasen.li@gmail.com>
2014-07-07 00:15:19 +0800

    添加基础配置文件类

---
+++ core/java/com/ximi/app/base/cofig/AppBaseConfig.java
@@ -0,0 +1,37 @@
+package com.ximi.app.base.cofig;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class AppBaseConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants arg0) {
+
+	}
+
+	@Override
+	public void configHandler(Handlers arg0) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors arg0) {
+
+	}
+
+	@Override
+	public void configPlugin(Plugins arg0) {
+
+	}
+
+	@Override
+	public void configRoute(Routes arg0) {
+
+	}
+
+}
