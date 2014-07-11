905dc9f6e1c015d9b5365f88d1d9afc45ec7ac73
Administrator <Administrator@192.168.56.1>
2014-07-11 22:50:14 +0800

    增加测试类

---
+++ core/src/test/java/core/TestDruidDatasouceUtil.java
@@ -0,0 +1,16 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.domeke.app.base.cofig.DruidDatasouceUtil;
+
+public class TestDruidDatasouceUtil {
+
+	@Test
+	public void testGetDruidPlugin() {
+		DruidDatasouceUtil.getDruidPlugin();
+	}
+
+}
