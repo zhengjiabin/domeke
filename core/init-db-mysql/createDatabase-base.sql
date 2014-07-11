ec579fcaa01362e0cd1150dbc96cdb7d59940938
Administrator <Administrator@192.168.56.1>
2014-07-12 00:07:06 +0800

    1

---
+++ core/init-db-mysql/createDatabase-base.sql
@@ -0,0 +1,9 @@
+#创建底层数据库表结构
+#001 用户表
+#userId,userKey,username, password, createdate,modifydate,fromdate,todate,status
+#002 人员表
+#personId,personKey,firstName,lastName, createDate,modifyDate,mobile,email
+#003 岗位表
+#positionId,positionKey,positionName,creater,createDate,modifier,modifyDate
+#004 文件存取路径
+#fileId,fileKey,fileName,host,filePath,creater,createDate,modifier,modifyDate,status
