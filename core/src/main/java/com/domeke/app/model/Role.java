1b81d3151a11430e92e285da5d74e7dae662fd78
Administrator <Administrator@192.168.56.1>
2014-07-23 23:01:09 +0800

    添加角色model

---
+++ core/src/main/java/com/domeke/app/model/Role.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.domeke.app.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * @author Administrator
+ * 角色
+ */
+public class Role extends Model<Role> {
+
+}
