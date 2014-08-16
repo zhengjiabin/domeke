605510544a006a7d44ca6e2dcddd8eb8f7ff490d
zhenjiabin <zhengjiabin6@126.com>
2014-08-16 17:30:00 +0800

    create tables

---
+++ core/init-db-mysql/domeke_activity.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE  IF NOT EXISTS `domeke` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `domeke`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: domeke
+-- ------------------------------------------------------
+-- Server version	5.6.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activity`
+--
+
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity` (
+  `activityid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `uid` mediumint(8) NOT NULL DEFAULT '0',
+  `aid` mediumint(8) NOT NULL DEFAULT '0',
+  `cost` mediumint(8) NOT NULL DEFAULT '0',
+  `starttimefrom` int(14) NOT NULL DEFAULT '0',
+  `starttimeto` int(14) NOT NULL DEFAULT '0',
+  `place` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `gender` tinyint(1) NOT NULL DEFAULT '0',
+  `number` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `applynumber` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `expiration` int(14) unsigned NOT NULL DEFAULT '0',
+  `ufield` text NOT NULL,
+  `credit` smallint(6) unsigned NOT NULL DEFAULT '0',
+  `status` tinyint(1) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creater` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modifier` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`activityid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activity`
+--
+
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-16 17:27:38
---
+++ core/init-db-mysql/domeke_treasure.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE  IF NOT EXISTS `domeke` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `domeke`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: domeke
+-- ------------------------------------------------------
+-- Server version	5.6.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `treasure`
+--
+
+DROP TABLE IF EXISTS `treasure`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `treasure` (
+  `treasureid` int(10) unsigned NOT NULL,
+  `author` varchar(15) NOT NULL,
+  `authorid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `subject` varchar(80) NOT NULL,
+  `dateline` int(10) unsigned NOT NULL DEFAULT '0',
+  `message` text NOT NULL,
+  `useip` varchar(15) NOT NULL,
+  `invisible` tinyint(1) NOT NULL DEFAULT '0',
+  `anonymous` tinyint(1) NOT NULL DEFAULT '0',
+  `usesig` tinyint(1) NOT NULL DEFAULT '0',
+  `attachment` tinyint(1) NOT NULL DEFAULT '0',
+  `rate` smallint(6) NOT NULL DEFAULT '0',
+  `ratetimes` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `status` int(10) NOT NULL DEFAULT '0',
+  `comment` tinyint(1) NOT NULL DEFAULT '0',
+  `replycredit` int(10) NOT NULL DEFAULT '0',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creater` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modifier` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`treasureid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `treasure`
+--
+
+LOCK TABLES `treasure` WRITE;
+/*!40000 ALTER TABLE `treasure` DISABLE KEYS */;
+/*!40000 ALTER TABLE `treasure` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-16 17:27:38
