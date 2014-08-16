b8f4028d8117ba63f18102554fc8fc3c837aa413
ericy0713@gmail.com <ericy0713@gmail.com>
2014-08-16 17:38:02 +0800

    商品模块创建表的SQL脚本，内含商品表，订单表

---
+++ core/init-db-mysql/goods-orders.sql
@@ -0,0 +1,37 @@
+
+DROP TABLE IF EXISTS pre_goods;
+CREATE TABLE IF NOT EXISTS pre_goods (
+goodsid	char(32) NOT NULL PRIMARY KEY,
+goods 	char(50) NOT NULL ,
+goodsname	varchar(255) NOT NULL ,
+price	float(7,2) unsigned NOT NULL ,
+amount	int(10) unsigned NOT NULL ,
+pic	varchar(255) NOT NULL ,
+message	varchar(255) NOT NULL ,
+tamllurl	varchar(255) NOT NULL ,
+submitdate	int(14) unsigned NOT NULL ,
+status	char(3) NOT NULL ,
+create_time  timestamp not null default current_timestamp ,
+creater	mediumint(8) unsigned NOT NULL ,
+modify_time	 timestamp not null default current_timestamp ,
+modifier	mediumint(8) unsigned NOT NULL 
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE IF NOT EXISTS orders (
+orderid	char(32) NOT NULL PRIMARY KEY,
+goodsid	char(32) NOT NULL ,
+status	char(3) NOT NULL,
+buyer	char(50) NOT NULL,
+uid	mediumint(8) unsigned NOT NULL,
+amount	int(10) unsigned NOT NULL,
+price	float(7,2) unsigned NOT NULL,
+submitdate	int(10) unsigned NOT NULL,
+confirmdate	int(10) unsigned NOT NULL,
+email	char(40) NOT NULL,
+ip	char(15) NOT NULL,
+create_time	timestamp not null default current_timestamp ,
+creater	mediumint(8) unsigned NOT NULL,
+modify_time	timestamp not null default current_timestamp ,
+modifier	mediumint(8) unsigned NOT NULL
+);
