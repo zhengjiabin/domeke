882b381c3342aaecc1203bb3b87a6f37b4ff70cd
zhenjiabin <zhengjiabin6@126.com>
2014-08-17 09:16:09 +0800

    修改数据库日期格式

--- core/init-db-mysql/domeke_activityapply.sql
+++ core/init-db-mysql/domeke_activityapply.sql
@@ -31,7 +31,7 @@ CREATE TABLE `activityapply` (
   `uid` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `message` varchar(255) NOT NULL,
   `verified` tinyint(1) NOT NULL DEFAULT '0',
-  `dateline` int(14) unsigned NOT NULL DEFAULT '0',
+  `dateline` timestamp NOT NULL ,
   `payment` mediumint(8) NOT NULL DEFAULT '0',
   `ufielddata` text NOT NULL,
   `gender` tinyint(1) NOT NULL DEFAULT '0',
