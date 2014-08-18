7cf6a306696581a5ab70dd022057a92fc33183e1
zhenjiabin <zhengjiabin6@126.com>
2014-08-19 00:14:59 +0800

    添加活动控制器

---
+++ core/src/main/java/com/domeke/app/controller/community/ActivityController.java
@@ -0,0 +1,56 @@
+package com.domeke.app.controller.community;
+
+import com.domeke.app.model.community.Activity;
+import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Page;
+
+public class ActivityController extends Controller {
+
+	/**
+	 * 查询所有活动信息
+	 * 
+	 * @return 活动信息
+	 */
+	public void find() {
+		int pageNumber = getParaToInt("activityPage_pageNumber", 1);
+		int pageSize = getParaToInt("activityPage_pageSize", 2);
+		Page<Activity> activityPage = Activity.dao.paginate(pageNumber,
+				pageSize, "select *", "from activity order by create_time");
+		setAttr("activityPage", activityPage);
+		render("/community/activity.html");
+	}
+
+	/**
+	 * 查询指定活动信息
+	 * 
+	 * @return 指定活动信息
+	 */
+	public void findById() {
+		int activityid = getParaToInt();
+		Activity activity = Activity.dao.findById(activityid);
+		setAttr("activity", activity);
+		render("/community/modifyActivity.html");
+	}
+
+	/**
+	 * 修改活动信息
+	 */
+	public void modify() {
+		Activity activityid = getModel(Activity.class);
+		activityid.update();
+		find();
+	}
+
+	/**
+	 * 删除指定活动
+	 */
+	public void deleteById() {
+		int activityid = getParaToInt();
+		Activity.dao.deleteById(activityid);
+		find();
+	}
+
+	public void index() {
+		render("/community/activity.html");
+	}
+}
