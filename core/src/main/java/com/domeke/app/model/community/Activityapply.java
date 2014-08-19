219a72c8943b007aed1c11524447443272340a4d
zhenjiabin <zhengjiabin6@126.com>
2014-08-19 22:08:15 +0800

    添加活动申请类

---
+++ core/src/main/java/com/domeke/app/model/community/Activityapply.java
@@ -0,0 +1,29 @@
+package com.domeke.app.model.community;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * CREATE TABLE `activityapply` (
+ * `activityapplyid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ * `activityid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ * `username` varchar(255) NOT NULL,
+ * `uid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ * `message` varchar(255) NOT NULL,
+ * `verified` tinyint(1) NOT NULL DEFAULT '0',
+ * `dateline` timestamp NOT NULL ,
+ * `payment` mediumint(8) NOT NULL DEFAULT '0',
+ * `ufielddata` text NOT NULL,
+ * `gender` tinyint(1) NOT NULL DEFAULT '0',
+ * `papers` tinyint(1) DEFAULT '0',
+ * `papersid` varchar(30) DEFAULT '0',
+ * `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ * `creater` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ * `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ * `modifier` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ * PRIMARY KEY (`activityapplyid`)
+ * ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ */
+public class Activityapply extends Model<Activityapply> {
+	public static Activityapply dao = new Activityapply();
+	
+}
