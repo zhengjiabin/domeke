2a1d999ebfd5e64a556b252e0813e2b4b94021be
zhouying-512@163.com <zy19880726>
2014-08-23 14:38:37 +0800

    zhouying  修改model

--- core/src/main/java/com/domeke/app/model/VentWall.java
+++ core/src/main/java/com/domeke/app/model/VentWall.java
@@ -6,13 +6,25 @@ package com.domeke.app.model;
 
 import java.util.List;
 
+import com.domeke.app.tablebind.TableBind;
 import com.jfinal.plugin.activerecord.Model;
 import com.jfinal.plugin.ehcache.CacheKit;
 
 /**
  * @author zhouying
- *
+ * DROP TABLE IF EXISTS `vent_wall`;
+ * CREATE TABLE `vent_wall` (
+ * `ventwallid` bigint(20) NOT NULL AUTO_INCREMENT,
+ * `userid` bigint(20) DEFAULT NULL,
+ * `moodid` bigint(20) DEFAULT NULL COMMENT '心情id',
+ * `message` varchar(255) DEFAULT NULL COMMENT '留言',
+ * `createtime`  TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+ * `creater` bigint(20) NULL,
+ * `modifytime` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ * `modifier` bigint(20) NULL,
+ * PRIMARY KEY (`ventwallid`));
  */
+@TableBind(tableName = "vent_Wall", pkName = "ventwallid")
 public class VentWall extends Model<VentWall>{
 	
 	/**
@@ -32,7 +44,7 @@ public class VentWall extends Model<VentWall>{
 	 * @return 所有留言
 	 */
 	public List<VentWall> getVentWall(){
-		List<VentWall> ventWallList = venWdao.findByCache("ventWallList", "key", "select * from ventwall order by create_time");
+		List<VentWall> ventWallList = venWdao.findByCache("ventWallList", "key", "select * from vent_wall order by createtime");
 		return ventWallList;	
 	}
 	/**
