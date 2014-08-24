760cac6fcd83b883a5cabf00c1f79993a9ef282c
_fcy123 <min927511>
2014-08-24 18:47:17 +0800

    审批商品

--- core/src/main/java/com/domeke/app/model/Approve.java
+++ core/src/main/java/com/domeke/app/model/Approve.java
@@ -6,6 +6,7 @@ package com.domeke.app.model;
 import java.util.List;
 
 import com.domeke.app.tablebind.TableBind;
+import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Model;
 
 /**
@@ -26,5 +27,29 @@ public class Approve extends Model<Approve> {
 	}
 	public boolean saveApprove(){
 		return this.save();
+	}
+	
+	public List<Goods> getGoods(){
+		String sql ="select * from goods where status != '40'";
+		Goods gooddao = new Goods();
+		List<Goods> goods = gooddao.find(sql);
+		return goods;
+	}
+	public void approveGoods(int id,String type){
+		Goods gooddao = new Goods();
+		gooddao =gooddao.findById(id);
+		if("Y".equals(type)){
+			String sql = "update goods set status='10' where goodsid = '"+id+"'";
+			Db.update(sql);
+		}else if("N".equals(type)){
+			String sql = "update goods set status='20' where goodsid = '"+id+"'";
+			Db.update(sql);
+		}else if("D".equals(type)){
+			String sql = "update goods set status='40' where goodsid = '"+id+"'";
+			Db.update(sql);
+		}
+	}
+	public void addApproveRecord(){
+		Goods gooddao = new Goods();
 	}
 }
