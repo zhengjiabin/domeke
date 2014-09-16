f62c0b0a73b054c47c123593d0616aaa4b8ed0bf
_fcy123 <min927511>
2014-09-16 23:21:11 +0800

    111

--- core/src/main/java/com/domeke/app/controller/LoginController.java
+++ core/src/main/java/com/domeke/app/controller/LoginController.java
@@ -51,7 +51,7 @@ public class LoginController extends Controller {
 			setCache(username, password, token, currentUser);
 			setAttr("username", username);
 			setAttr("menuid", "1");
-			render("/admin/community.html");
+			render("/admin/admin_community.html");
 			return;
 		}
 		setCache(username, password, token, currentUser);
