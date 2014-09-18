f1cdbc983bd7b76c84471305ecafdfeb1df1204e
lijiasen@domeke.com <lijiasen@domeke.com@192.168.56.1>
2014-09-18 21:30:26 +0800

    3

---
+++ core/init-db-mysql/02_initdb.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO domeke.user
+(userid,username, password, email, mobile, create_time, creater, modifier, modify_time,activation)
+VALUES('1','admin', 'ISMvKXpXpadDiUoOSoAfww==', '7061089@qq.com', '11111111111', CURRENT_TIMESTAMP, 'admin', 'admin', CURRENT_TIMESTAMP,'Y');
+
+INSERT INTO domeke.role(roleid,rolename) VALUES('1','user');
+INSERT INTO domeke.role(roleid,rolename) VALUES('2','admin');
+INSERT INTO domeke.role(roleid,rolename) VALUES('3','guest');
+
+
+
+
+
+
+
