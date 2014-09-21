958f7049ed5ff4d989a816b644dd7d6ee9c9a03c
zhouying-512@163.com <zhouying-512@163.com>
2014-09-21 17:55:50 +0800

    优化

---
+++ core/src/main/webapp/js/searchkeymanage.js
@@ -0,0 +1,48 @@
+$(function(){
+    $(document).ready(function(){
+        $("button[lable='修改']").click(function(){
+          var $self = $(this);
+          var $parent = $self.parent();
+          $parent = $parent.parent();
+          var keyname =$('td input',$parent).val();
+          var keyid = $(this).attr("name");
+          console.log(keyname+"Gogo"+keyid);
+          var obj = {"searchKey.keyid":keyid,"searchKey.keyname":keyname};
+          var url = "searchkey/updateById/"+keyid;
+          $.showView(obj,url); 
+      });
+        $("button[lable='删除']").click(function(){
+          var $self = $(this);
+          var $parent = $self.parent();
+          $parent = $parent.parent();
+          var keyid = $(this).attr("name");
+          console.log(keyid);
+          var obj = {"searchKey.keyid":keyid};
+          var url = "searchkey/delete/"+keyid;
+          $.showView(obj,url); 
+      });
+        $("button[lable='新增']").click(function(){
+        	var keyname = $('#keyname1').val();
+            console.log(keyname);
+            var obj = {"searchKey.keyname":keyname};
+            var url = "searchkey/save";
+            $.showView(obj,url); 
+        });
+    });       
+          $.extend({
+           	showView:function(obj,url){
+        	$.ajax({   
+                    type:"post",    
+                    dataType:"html",
+                    url:url, 
+                    data:obj,
+                    success:function(data){
+                    	$('#content').html(data);
+                    },   
+                    error:function(){ 
+                        alert("error");
+                    }   
+                });
+        	}
+       });  	
+})
--- core/src/main/webapp/js/ventwall.js
+++ core/src/main/webapp/js/ventwall.js
@@ -1,5 +1,6 @@
 $(function() {
 	$(document).ready(function(){
+		$('#wish').wish();
 		var url = "ventwall/select";
 		$('#btn2').click(function(){
 			var rdo = $('input[name="ventWall.moodid"]:checked').val();
@@ -8,29 +9,16 @@ $(function() {
 			var url = "ventwall/save";
 			$.showView(obj,url);
 		});
-	    $("button[lable='删除']").click(function(){
-	        var $self = $(this);
-	        var $parent = $self.parent();
-	        $parent = $parent.parent();
-	        var ventwallid = $(this).attr("name");
-	        console.log(ventwallid);
-	        var obj = {"ventWall.ventwallid":ventwallid};
-	        var url = "ventwall/delete/"+ventwallid;
-	        var ref = "#content";
-	        console.log(ref);
-	        $.showView(obj,url,ref); 
-	    });  
-	});      
+	});       
 	$.extend({
-		showView:function(obj,url,ref){
+		showView:function(obj,url){
 			$.ajax({   
                 type:"post",    
                 dataType:"html",
                 url:url, 
                 data:obj,
                 success:function(data){
-                	console.log("kk"+data);
-                	$(ref).html(data);                  	
+                	$('#view').html(data);                  	
                 },   
                 error:function(){ 
                 	alert("error");
@@ -43,7 +31,7 @@ $(function() {
         function maxLimit(){
                 var num=$(this).val().substr(0,50);
                 $(this).val(num);
-                $(this).next().children("span").text($(this).val().length+"/50");
+                $(this).next().text($(this).val().length+"/50");
         };
         $("#saytext").keyup(maxLimit);
 });
