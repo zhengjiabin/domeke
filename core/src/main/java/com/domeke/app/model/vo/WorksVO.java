5f3547533c34e51bbec17c443053d6e4a65ca1d9
lijiasen@domeke.com <lijiasen@domeke.com@192.168.56.1>
2014-09-21 15:58:01 +0800

    优化SolrKit

---
+++ core/src/main/java/com/domeke/app/model/vo/WorksVO.java
@@ -0,0 +1,30 @@
+package com.domeke.app.model.vo;
+
+public class WorksVO extends BaseVO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3683462974089424783L;
+
+	private String worksname;
+
+	private String describle;
+
+	public String getWorksname() {
+		return worksname;
+	}
+
+	public void setWorksname(String worksname) {
+		this.worksname = worksname;
+	}
+
+	public String getDescrible() {
+		return describle;
+	}
+
+	public void setDescrible(String describle) {
+		this.describle = describle;
+	}
+
+}
