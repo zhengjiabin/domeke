1c04bcd3f765b778da528c9c9327cbfee220c7ea
zhenjiabin <zhengjiabin6@126.com>
2014-09-27 17:14:56 +0800

    modify treasure

--- core/init-db-mysql/01_domeke_database.sql
+++ core/init-db-mysql/01_domeke_database.sql
@@ -445,42 +445,22 @@ CREATE TABLE `role` (
 -- ----------------------------
 DROP TABLE IF EXISTS `treasure`;
 CREATE TABLE `treasure` (
-  `treasureid` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `author` varchar(15) NOT NULL,
-  `authorid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `treasureid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `subject` varchar(80) NOT NULL,
-  `dateline` int(10) unsigned NOT NULL DEFAULT '0',
+  `userid` bigint(20) NOT NULL DEFAULT '0',
+  `dateline` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `message` text NOT NULL,
-  `useip` varchar(15) NOT NULL,
-  `invisible` tinyint(1) NOT NULL DEFAULT '0',
-  `anonymous` tinyint(1) NOT NULL DEFAULT '0',
-  `usesig` tinyint(1) NOT NULL DEFAULT '0',
-  `attachment` tinyint(1) NOT NULL DEFAULT '0',
-  `rate` smallint(6) NOT NULL DEFAULT '0',
-  `ratetimes` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `status` int(10) NOT NULL DEFAULT '0',
-  `comment` tinyint(1) NOT NULL DEFAULT '0',
-  `replycredit` int(10) NOT NULL DEFAULT '0',
-  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `creater` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `modifytime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modifier` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`treasureid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for treasure_approve
--- ----------------------------
-DROP TABLE IF EXISTS `treasure_approve`;
-CREATE TABLE `treasure_approve` (
-  `treasureapproveid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `treasureid` bigint(20) unsigned NOT NULL,
-  `status` tinyint(3) NOT NULL DEFAULT '0',
+  `userip` varchar(15) NOT NULL,
+  `times` bigint(20) DEFAULT '0',
+  `top` tinyint(1) NOT NULL DEFAULT '0',
+  `essence` tinyint(1) NOT NULL DEFAULT '0',
+  `status` varchar(4) NOT NULL DEFAULT '10',
+  `communityid` bigint(20) unsigned NOT NULL DEFAULT '0',
   `createtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `creater` bigint(20) DEFAULT NULL,
   `modifier` bigint(20) DEFAULT NULL,
   `modifytime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`treasureapproveid`)
+  PRIMARY KEY (`treasureid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
