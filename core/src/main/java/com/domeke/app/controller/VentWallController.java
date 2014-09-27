d936eabc3894fe69c4bba8032be05ffe0cb65587
zhouying-512@163.com <zhouying-512@163.com>
2014-09-27 17:04:01 +0800

    发泄墙优化

--- core/src/main/java/com/domeke/app/controller/VentWallController.java
+++ core/src/main/java/com/domeke/app/controller/VentWallController.java
@@ -34,10 +34,7 @@ public class VentWallController extends Controller {
 	 * 新增留言记录
 	 */
 	public void save(){
-		VentWall ventWall = getModel(VentWall.class);
-		String msg = ventWall.getStr("moodid");
-		msg = getImg(msg);
-		ventWall.set("moodid", msg);	
+		VentWall ventWall = getModel(VentWall.class);	
 		ventWall.set("creater", 1);
 		Long userId = getUser();
 		ventWall.set("userid", userId);
@@ -45,21 +42,6 @@ public class VentWallController extends Controller {
 		select();
 		render("/VentWallDtl.html");
 	}
-	/**
-	 * 正则表达式转换表情
-	 * @param msg
-	 * @return 已转换格式
-	 */
-	private String getImg(String msg) {
-		Pattern p=Pattern.compile("\\[em_(\\d+)\\]");
-		Matcher matcher=p.matcher(msg);
-		while(matcher.find()){
-			String imgNo=matcher.group(1);
-			msg = msg.replace("[em_"+imgNo+"]",imgNo +".jpg");
-		}
-		return msg;
-	}
-	
 	public void index() {			
 		int menuid = getParaToInt("menuid");
 		setAttr("menuid", menuid);	
