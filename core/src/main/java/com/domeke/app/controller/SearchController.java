b775f4fc8d3fdd9f911067b24212c7f8c76ffd5a
lijiasen@domeke.com <lijiasen@domeke.com@WIN-4F3N18K149L>
2014-10-11 21:39:59 +0800

    全文检索添加page测试

--- core/src/main/java/com/domeke/app/controller/SearchController.java
+++ core/src/main/java/com/domeke/app/controller/SearchController.java
@@ -9,6 +9,7 @@ import com.domeke.app.model.vo.WorksVO;
 import com.domeke.app.route.ControllerBind;
 import com.domeke.app.solr.utils.SolrKit;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Page;
 
 /**
  * 站内搜索
@@ -23,11 +24,12 @@ public class SearchController extends Controller {
 	 * 站内搜索功能
 	 */
 	public void search() {
+		Page page = new Page(null, 0, 0, 0, 0);
 		String queryKey = getPara("queryKey");
 		WorksVO worksVO = new WorksVO();
 		try {
-			String[] tags = new String[] { "worksname", "describle", "cover" };
-			List<BaseVO> worksVOList = SolrKit.query(tags, queryKey, worksVO);
+			String[] tags = new String[] { "worksname", "desc", "cover" };
+			List<BaseVO> worksVOList = SolrKit.query(tags, queryKey, worksVO, page);
 			setAttr("worksVOList", worksVOList);
 			render("/searchSite.html");
 		} catch (SolrServerException e) {
