d5afd9e24b880a7c16778231cc4bd40fd1d635fe
_fcy123 <min927511>
2014-10-12 17:34:14 +0800

    11

--- core/src/main/java/com/domeke/app/model/DownLoad.java
+++ core/src/main/java/com/domeke/app/model/DownLoad.java
@@ -1,17 +1,17 @@
 package com.domeke.app.model;
 
-import java.util.List;
-
 import com.domeke.app.tablebind.TableBind;
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Page;
 
 @TableBind(tableName="downloadcount", pkName="DownLoadCountId")
 public class DownLoad extends Model<DownLoad>{
 	public static DownLoad down = new DownLoad();
 	
-	public List<DownLoad> getByUserId(Long userid){
-		String sql="select * from downloadcount where userid = '"+userid+"'";
-		List<DownLoad> downList = this.find(sql);
-		return downList;
-	}
+	
+	
+	  public Page<DownLoad> getByUserId(Long userId,int pageNumber,int pageSize){
+	    	Page<DownLoad> page = this.paginate(pageNumber, pageSize, "select * ", "from downloadcount where userid =?",userId);
+	    	return page;
+	    }
 }
