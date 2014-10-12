c59704666cb7882b8a8e068c1afa4789297a2c8c
_fcy123 <min927511>
2014-10-12 10:34:23 +0800

    我的下载

---
+++ core/src/main/java/com/domeke/app/controller/DownLoadController.java
@@ -0,0 +1,11 @@
+package com.domeke.app.controller;
+
+import com.domeke.app.route.ControllerBind;
+import com.jfinal.core.Controller;
+
+@ControllerBind(controllerKey="down")
+public class DownLoadController extends Controller{
+	public void getMyDownLoad(){
+		
+	}
+}
