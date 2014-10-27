cb4dafce0016457d6a3a993300f268fbc32db271
zhouying-512@163.com <zhouying-512@163.com>
2014-10-27 20:47:37 +0800

    首页关键字

--- core/src/main/java/com/domeke/app/interceptor/GoodsTypeInterceptor.java
+++ core/src/main/java/com/domeke/app/interceptor/GoodsTypeInterceptor.java
@@ -3,6 +3,7 @@ package com.domeke.app.interceptor;
 import java.util.List;
 
 import com.domeke.app.model.GoodsType;
+import com.domeke.app.model.SearchKey;
 import com.jfinal.aop.Interceptor;
 import com.jfinal.core.ActionInvocation;
 import com.jfinal.core.Controller;
@@ -18,6 +19,8 @@ public class GoodsTypeInterceptor implements Interceptor  {
 		List<GoodsType> topGoodsTypeList = GoodsType.gtDao.getTopGoodsType();
 		Controller controller = ai.getController();
 		controller.setAttr("topGoodsTypeList", topGoodsTypeList);
+		List<SearchKey> searchKeyList = SearchKey.searchdao.getSearchKey();
+		controller.setAttr("searchKeyList", searchKeyList);
 		ai.invoke();
 	}
 
