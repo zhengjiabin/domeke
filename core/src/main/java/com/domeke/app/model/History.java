373938fc79a669d99e7b3d2d15cb28e331420139
Administrator <Administrator@luyu-PC>
2014-10-28 22:56:06 +0800

    history

--- core/src/main/java/com/domeke/app/model/History.java
+++ core/src/main/java/com/domeke/app/model/History.java
@@ -1,9 +1,11 @@
 package com.domeke.app.model;
 
 import java.time.LocalDate;
+import java.util.Date;
 
+import com.domeke.app.tablebind.TableBind;
 import com.jfinal.plugin.activerecord.Model;
-
+@TableBind(tableName="history", pkName="hid")
 public class History extends Model<History> {
 
 	/**
@@ -14,33 +16,36 @@ public class History extends Model<History> {
 
 	public void saveOrUpdateHitory(Work work) {
 		History history = new History();
-		history.set("workid", work.get("workid"));
-		history.set("worksid", work.get("worksid"));
-		history.set("workname", work.get("workname"));
-		history.set("cover", work.get("cover"));
-		history.set("comic", work.get("comic"));
+	
 		LocalDate now = LocalDate.now();
 
 		Works dao = new Works();
 		Works works = dao.findById(work.get("worksid"));
 		works.set("pageviews", works.getLong("pageviews") + 1);
 		works.update();
-		history = getWeekHistory(work, now);
-		if (history != null && history.getLong("count") > 0) {
+		history = getDayHistory(work, now);
+		if (history.isNotEmpty() && history.getLong("count") > 0) {
 			Long count = history.getLong("count") + 1;
 			history.set("count", count);
 			history.update();
 		} else {
+			history.set("workid", work.get("workid"));
+			history.set("worksid", work.get("worksid"));
+			history.set("workname", work.get("workname"));
+			history.set("cover", work.get("cover"));
+			history.set("comic", work.get("comic"));
 			history.set("count", 1);
+			history.set("fromtime",new Date());
+			history.set("totime",new Date());
 			history.save();
 		}
 	}
 
-	private History getWeekHistory(Work work, LocalDate now) {
+	private History getDayHistory(Work work, LocalDate now) {
 		StringBuffer buffer = new StringBuffer("select *  from history  ");
-		buffer.append(" where fromtime <= str_to_date(?,'%Y-%m-%d') and totime >= str_to_date(?,'%Y-%m-%d') ");
+		buffer.append(" where  date_format(fromtime,'%Y-%m-%d') = ? ");
 		buffer.append(" and workid = ?");
-		History find = dao.findFirst(buffer.toString(), now.toString(), now.toString(), work.get("workid"));
-		return find;
+		History find = dao.findFirst(buffer.toString(), now.toString(), work.get("workid"));
+		return find == null ?  new History():find;
 	}
 }
