676f71b321906f61f546eb928adec98cfb177a93
zhenjiabin <zhengjiabin6@126.com>
2014-11-08 17:36:54 +0800

    修改登录校验的获取路径问题

--- core/src/main/java/com/domeke/app/interceptor/LoginInterceptor.java
+++ core/src/main/java/com/domeke/app/interceptor/LoginInterceptor.java
@@ -1,25 +1,39 @@
 package com.domeke.app.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
+
 import com.jfinal.aop.Interceptor;
 import com.jfinal.core.ActionInvocation;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
 
 public class LoginInterceptor implements Interceptor {
 	public void intercept(ActionInvocation ai) {
 		try {
 			Controller controller = ai.getController();
 			HttpServletRequest request = controller.getRequest();
-			String contentPath = request.getLocalAddr() +":"+ request.getLocalPort() + request.getContextPath();
 			if (controller.getSessionAttr("user") == null) {
 				controller.setAttr("msg", "需要登录才可以进行该操作！");
-				controller.renderHtml("<script> window.location.href = 'http://"+contentPath+"/user/goLogin';</script>");
+				String basePath = getbasePath(request);
+				controller.renderHtml("<script> window.location.href = '" + basePath + "/user/goLogin';</script>");
 				return;
 			}
 			ai.invoke();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	/**
+	 * 获取根路径
+	 * @param request
+	 * @return
+	 */
+	private String getbasePath(HttpServletRequest request){
+		StringBuffer url = request.getRequestURL();
+		String uri = request.getRequestURI();
+		String basePath = StrKit.replace(url.toString(), uri, "");
+		basePath = basePath + request.getContextPath();
+		return basePath;
 	}
 }
