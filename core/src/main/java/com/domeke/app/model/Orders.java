4fcc58b7f0310e407eee4a8fdc9322ad02d79cef
zhouying-512@163.com <zhouying-512@163.com>
2014-11-15 12:09:28 +0800

    发泄墙相关修改

--- core/src/main/java/com/domeke/app/model/Orders.java
+++ core/src/main/java/com/domeke/app/model/Orders.java
@@ -9,7 +9,7 @@ public class Orders extends Model<Orders>{
 	private static Orders orders = new Orders();
 	
 	public Page<Orders> getAllOrders(int pageNumber,int pageSize){
-		Page<Orders> page = this.paginate(pageNumber, pageSize, "select * ", "from orders order by creattime");
+		Page<Orders> page = this.paginate(pageNumber, pageSize, "select * ", "from orders order by creattime desc");
 		return page;
 	}
 	
