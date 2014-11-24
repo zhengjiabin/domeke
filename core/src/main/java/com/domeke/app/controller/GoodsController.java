73fcf9f215a6c71c6d401c56d0ecf7e63e916125
陈智聪 <chenzhicong>
2014-11-24 20:19:21 +0800

    修改主图编辑时被删除问题

--- core/src/main/java/com/domeke/app/controller/GoodsController.java
+++ core/src/main/java/com/domeke/app/controller/GoodsController.java
@@ -186,15 +186,14 @@ public class GoodsController extends FilesLoadController {
 		String saveDirectory = headimg.substring(headimg.lastIndexOf("/") + 1, headimg.length());
 		saveDirectory = saveFolderName + "/" + saveDirectory;
 		Map<String, String> uploadPath = FileLoadKit.uploadImgs(this, saveDirectory, maxPostSize, encoding);
-		String uppic = pic;
 		if (uploadPath != null && uploadPath.get("pic") != null) {
-			uppic = uploadPath.get("pic");
+			FileLoadKit.deleteFiles(this, pic);
+			pic = uploadPath.get("pic");
 		}
 		UploadFile file = getFile();
 		Goods gs = getModel(Goods.class);
-		gs.set("pic", uppic);
+		gs.set("pic", pic);
 		try{
-			FileLoadKit.deleteFiles(this, pic);
 			gs.updateGoodsInfo();
 			redirect("/goods/renderGoods");
 		} catch (Exception e) {
