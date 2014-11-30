79058787251dc35792cd13e026110db1e2f2392d
lijiasen@domeke.com <lijiasen@domeke.com@WIN-4F3N18K149L.lan>
2014-11-30 16:27:54 +0800

    修改截取标题逻辑

--- core/src/main/java/com/jfinal/kit/ParseDemoKit.java
+++ core/src/main/java/com/jfinal/kit/ParseDemoKit.java
@@ -9,18 +9,18 @@ import com.domeke.app.model.Works;
 import com.google.common.collect.Lists;
 
 public class ParseDemoKit {
-	public static List<Map<String, Object>> worksParse(List<Works> workss){
+	public static List<Map<String, Object>> worksParse(List<Works> workss) {
 		List<Map<String, Object>> resultMap = Lists.newArrayList();
 		try {
 			for (Works works : workss) {
-				if(works == null)
+				if (works == null)
 					continue;
 				Map<String, Object> map = new HashMap<String, Object>();
 				Object worksid = works.get("worksid");
 				map.put("worksid", worksid);
 				String worksname = works.get("worksname");
-				if(worksname.length() > 10){
-					worksname = worksname.substring(0,10);
+				if (worksname.length() > 10) {
+					worksname = worksname.substring(0, 10);
 				}
 				map.put("worksname", worksname);
 				map.put("workstype", works.get("workstype"));
@@ -32,27 +32,27 @@ public class ParseDemoKit {
 				String playUrl = "";
 				String detailUrl = "";
 				Integer type = works.getInt("type");
-				if(type == 1){
-					//0专辑 
-					playUrl = "cartoon/playVideo?id="+worksid;
-					detailUrl = "cartoon/showDetail?id="+worksid;
-				}else if(type == 2){
-					 //视频
-					playUrl = "manhua/playVideo?id="+worksid;
-					detailUrl = "manhua/showDetail?id="+worksid;
+				if (type == 1) {
+					// 0专辑
+					playUrl = "cartoon/playVideo?id=" + worksid;
+					detailUrl = "cartoon/showDetail?id=" + worksid;
+				} else if (type == 2) {
+					// 视频
+					playUrl = "manhua/playVideo?id=" + worksid;
+					detailUrl = "manhua/showDetail?id=" + worksid;
 				}
-				map.put("playUrl",playUrl);
-				map.put("detailUrl",detailUrl);
-				String typeUrl = "cartoon/showWorksList?workstype="+works.get("workstype");
-				map.put("typeUrl",typeUrl);
+				map.put("playUrl", playUrl);
+				map.put("detailUrl", detailUrl);
+				String typeUrl = "cartoon/showWorksList?workstype=" + works.get("workstype");
+				map.put("typeUrl", typeUrl);
 				String describle = works.get("describle");
-				if(describle.length() > 10){
+				if (describle.length() > 10) {
 					describle = describle.substring(0, 10) + "...";
 				}
-				if(StrKit.isBlank(describle)){
+				if (StrKit.isBlank(describle)) {
 					describle = "";
 				}
-				map.put("describle",describle);
+				map.put("describle", describle);
 				map.put("creater", works.get("createtime"));
 				map.put("creatername", works.get("creatername"));
 				map.put("createtime", works.get("createtime"));
@@ -67,18 +67,18 @@ public class ParseDemoKit {
 		}
 		return resultMap;
 	}
-	
-	public static List<Map<String, Object>> goodsParse(List<Goods> goodss){
+
+	public static List<Map<String, Object>> goodsParse(List<Goods> goodss) {
 		List<Map<String, Object>> resultMap = Lists.newArrayList();
 		try {
 			for (Goods goods : goodss) {
-				if(goods == null)
+				if (goods == null)
 					continue;
 				Map<String, Object> map = new HashMap<String, Object>();
 				map.put("goodsid", goods.get("goodsid"));
 				String goodsname = goods.get("goodsname");
-				if(goodsname.length() >= 10){
-					goodsname = goodsname.substring(0,12);
+				if (goodsname.length() >= 12) {
+					goodsname = goodsname.substring(0, 12);
 				}
 				map.put("goodsname", goodsname);
 				map.put("price", goods.get("price"));
@@ -89,7 +89,7 @@ public class ParseDemoKit {
 				map.put("dougprice", goods.get("dougprice"));
 				map.put("showflag", goods.get("showflag"));
 				String message = goods.get("message");
-				if(message.length() > 50){
+				if (message.length() > 50) {
 					message = message.substring(0, 50) + "...";
 				}
 				map.put("message", message);
@@ -97,7 +97,9 @@ public class ParseDemoKit {
 				map.put("username", goods.get("username"));
 				map.put("headimg", goods.get("headimg"));
 				map.put("sumlove", goods.get("sumlove"));
-				String detailUrl = "goods/getGoodsDetail?goodsid="+goods.get("goodsid")+"&goodsattr="+goods.get("goodsattr1")+"&dougprice="+goods.get("dougprice")+"&headimg="+goods.get("headimg");
+				String detailUrl = "goods/getGoodsDetail?goodsid=" + goods.get("goodsid") + "&goodsattr="
+						+ goods.get("goodsattr1") + "&dougprice=" + goods.get("dougprice") + "&headimg="
+						+ goods.get("headimg");
 				map.put("detailUrl", detailUrl);
 				resultMap.add(map);
 			}
@@ -106,5 +108,5 @@ public class ParseDemoKit {
 		}
 		return resultMap;
 	}
-	
+
 }
