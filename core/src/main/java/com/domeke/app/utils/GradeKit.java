ab289b2574cb6e7488527caa4d08d05930571a78
lijiasen@domeke.com <lijiasen@domeke.com@WIN-4F3N18K149L.lan>
2014-11-30 15:48:34 +0800

    签到等级修改

--- core/src/main/java/com/domeke/app/utils/GradeKit.java
+++ core/src/main/java/com/domeke/app/utils/GradeKit.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import com.domeke.app.model.CodeTable;
 import com.domeke.app.model.User;
+
 /**
  * @author zhouying
  */
@@ -14,28 +15,34 @@ public class GradeKit {
 	 * 获得等级
 	 * @param user对象
 	 */
-	public static Map<String,Object> getGrade(User user){
-		Map<String,Object> gradeMap = new HashMap<String,Object>();
+	public static Map<String, Object> getGrade(User user) {
+		Map<String, Object> gradeMap = new HashMap<String, Object>();
 		List<CodeTable> codetables = CodeKit.getList("grade");
-		//豆子
+		// 豆子
 		Long peas = 0L;
-		//等级
+		// 等级
 		Long point = 0L;
-		//等级名
+		// 等级名
 		String pointName = "";
-		if (user.get("peas") != null || user.get("point") != null){
-			peas = user.get("peas");	
+		if (user.get("peas") != null || user.get("point") != null) {
+			peas = user.get("peas");
 			point = user.get("point");
 		}
-		for (int i = 1; i <= codetables.size(); i++){
+		for (int i = 1; i <= codetables.size(); i++) {
 			int codekey = Integer.parseInt(codetables.get(i).get("codekey"));
-			if (point <= codekey){
-				pointName = codetables.get(i-1).getStr("codename");
+			int startPoint = 0;
+			if (i == 1) {
+				startPoint = 0;
+			} else {
+				startPoint = Integer.parseInt(codetables.get(i - 1).get("codekey"));
+			}
+			if (point <= codekey && point >= startPoint) {
+				pointName = codetables.get(i).getStr("codename");
 				gradeMap.put("pointName", pointName);
 				gradeMap.put("poin", point);
 				gradeMap.put("peas", peas);
 				break;
-			}			
+			}
 		}
 		return gradeMap;
 	}
